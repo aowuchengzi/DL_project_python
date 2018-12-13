4c060512113bf962fdc4b55e16073a6f32243d45
aowuchengzi <1533971306@qq.com>
2018-12-13 10:37:01 +0800

    修改

--- python_project/Benign_and_malignant_detection_of_thyroid_nodules/data_process.py
+++ python_project/Benign_and_malignant_detection_of_thyroid_nodules/data_process.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Nov 29 14:43:25 2018
 
-@author: 15339
+@author: 叶晨
 """
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -19,8 +19,8 @@ folder_path = r'图像标签\PixelLabelData'
 save_folder_path = r'图像标签\label'
 #%%
 def visual_mask(folder_path, save_folder_path, file_type = '.png'):
+    #把matlab生成的0-1掩模可视化
     '''
-    把matlab生成的0-1掩模可视化
     folder_path:目标文件夹
     save_folder_path:处理后的保存文件夹
     file_type：目标文件夹中图片类型
@@ -34,3 +34,13 @@ def visual_mask(folder_path, save_folder_path, file_type = '.png'):
         img_1 = Image.fromarray(x, 'L')
         save_path = os.path.join(save_folder_path, file_name[1])
         img_1.save(save_path)   #保存到指定文件夹。
+#%%        
+def image_resize(folder_path, save_folder_path, target_size = (512,512),  file_type = '.png'):
+    # 图片改变尺寸并保存
+    for path in getAllImages(folder_path, file_type):
+        #读图
+        file_name = os.path.split(path)
+        img = Image.open(path)
+        img = img.resize(target_size)
+        save_path = os.path.join(save_folder_path, file_name[1])
+        img.save(save_path)   #保存到指定文件夹。
